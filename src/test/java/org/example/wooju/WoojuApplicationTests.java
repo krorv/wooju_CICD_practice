4a15c141686a7e8493108ec35663c30f6d568472
01 <parkjua78@gmail.com>
2026-03-09 16:34:21 +0900

    init

---
+++ src/test/java/org/example/wooju/WoojuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wooju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoojuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
